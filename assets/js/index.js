27364c300c3bd4efeb66f5ec4aeee21cf32b4f54
Shubham - ardent <shubham.chavan@ardentisys.com>
2024-12-05 18:03:15 +0530

    added js

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,6 @@ const subjectElement = document.getElementById("subject");
 const grid = document.querySelector(".name-wrapper");
 const submitBtn = document.querySelector("#submit-btn");
 const outputContainer = document.querySelector(".output-display");
-
 const contacts = document.querySelector(".contact-list");
 // errors
 const firstNameError = document.querySelector(".error-fname");
@@ -17,6 +16,7 @@ const emailError = document.querySelector(".error-ename");
 const subjectError = document.querySelector(".error-subject");
 
 const form = document.querySelector(".contactform");
+
 let data = [];
 
 form.addEventListener("click", (event) => {
@@ -24,7 +24,7 @@ form.addEventListener("click", (event) => {
 
   const phonePattern = /^(?:\+91|91)?[6-9][0-9]{9}$/;
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+  const namePattern = /^[a-zA-Z]+$/;
   //values
   let fname = firstName.value;
   let lname = lastName.value;
@@ -34,10 +34,15 @@ form.addEventListener("click", (event) => {
 
   const isEmailValid = emailPattern.test(emailData);
   const isPhoneValid = phonePattern.test(phone);
+  const isNameValid1 = namePattern.test(fname);
+  const isNameValid2 = namePattern.test(lname);
 
   let isValid = true;
 
   if (fname.length < 3) {
+    firstNameError.classList.add("visible");
+    isValid = false;
+  } else if (!isNameValid1) {
     firstNameError.classList.add("visible");
     isValid = false;
   } else {
@@ -45,6 +50,9 @@ form.addEventListener("click", (event) => {
   }
 
   if (lname.length < 3) {
+    lastNameError.classList.add("visible");
+    isValid = false;
+  } else if (!isNameValid2) {
     lastNameError.classList.add("visible");
     isValid = false;
   } else {
@@ -82,10 +90,11 @@ form.addEventListener("click", (event) => {
       contactNumber: phone,
       subjectData: subject,
     };
+
     data.push(Details);
-    //render data
+    console.log(data, "after pushing in data array");
+    localStorage.setItem("contacts", JSON.stringify(data));
     dataRender();
-    console.log(data);
 
     // make form values empty
     firstName.value = "";
@@ -96,20 +105,28 @@ form.addEventListener("click", (event) => {
   }
 });
 
+// renders data
 function dataRender() {
   contacts.innerHTML = "";
-  data.map((item, index) => {
+  const contactDetails = JSON.parse(localStorage.getItem("contacts"));
+  console.log(contactDetails, "from local storage");
+  
+  if (contactDetails.length !== 0) {
     outputContainer.style.display = "flex";
+  }
+
+  contactDetails.map((item, index) => {
     // list creation
     const listitem = document.createElement("li");
     // adding class to each list
     listitem.className = "heading";
     listitem.innerHTML = `<span>${item.firstName}</span>
-                    <span>${item.lastName}</span>
-            
-                    <span>${item.email}</span>
-                    <span>${item.contactNumber}</span>`;
+                          <span>${item.lastName}</span>
+                          <span>${item.email}</span>
+                          <span>${item.contactNumber}</span>`;
 
     contacts.appendChild(listitem);
   });
 }
+
+// localStorage.clear();
